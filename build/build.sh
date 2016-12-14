bfc186ba3ae4a17da19d129fc9f8ae2367b87e85
babyeric <babyeric1982@gmail.com>
2016-12-14 14:52:34 -0800

    add docker login in build script

--- build/build.sh
+++ build/build.sh
@@ -10,5 +10,13 @@ zip -r ./build/image/web.zip ./web
 
 echo create docker image...
 cd ./build/image && docker build -t "$MY_ECR":"$MY_VER" .
+
+CMD_DOCKER_LOGIN=`aws ecr get-login --region us-west-2`
+echo docker login...
+$CMD_DOCKER_LOGIN
+
+echo tag image on ECR...
 docker tag "$MY_ECR":"$MY_VER" 366301983362.dkr.ecr.us-west-2.amazonaws.com/test_repo:"$MY_VER"
+
+echo push image to ECR...
 docker push 366301983362.dkr.ecr.us-west-2.amazonaws.com/test_repo:"$MY_VER"
